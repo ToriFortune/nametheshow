f27dc237bf8fdeeee96ef91c7a46d5bc1e23ccd8
Tori Fortune <toriolley@gmail.com>
2019-03-13 05:56:16 -0500

    "updated javascript"

---
+++ assets/javascript/sleuthing.js
@@ -0,0 +1,9 @@
+//The object of the game is to correctly guess and spell out a show from the 80s....die hard shepherd 
+
+alert("Would you like to walk down memory lane?");
+alert("You have 15 total guesses to name this show!")
+
+var wins = 0;
+var losses = 0;
+
+console.log ("Game dot script file");
