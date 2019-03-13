882797c5b487b92425231b00d850d927f5728fc5
Tori Fortune <toriolley@gmail.com>
2019-03-13 11:26:14 -0500

    "updated js, continue from syntax break syntax error"

--- assets/javascript/sleuthing.js
+++ assets/javascript/sleuthing.js
@@ -1,10 +1,12 @@
 //The object of the game is to correctly guess and spell out a show from the 80s....die hard shepherd 
-//created html doc
+//This game is a modified form of hangman 
+//Single player game, player has fifteen chances to guess the name of a show from the 80s
 
 
 alert("You have 15 total guesses to name this show!");
 
 var name = prompt ("What is your name?");
+if (name) alert("Hi " + "there " +name);
 console.log ("Hi " +  "there " + name);
 var memorylane = confirm ("Would you like to walk down memory lane?");
 if (memorylane)
@@ -15,6 +17,25 @@ if (memorylane)
     else {
         alert("Too bad, maybe next time!")
     }
+    var gameguessword =[
+        "moonlighting"
+    ];
+    var answerword =[];
+    for (var i = 0; i < gameguessword.length; i++){
+        answerword [i] ="__"
+        var pendingalphabets = word.length; }
+        var playersguess = prompt ("Pick a letter from the options on the screen. If you would like to end game, select cancel");
+        
+        if (playersguess === null);
+        { break;}
+       
+     else (gameguessword.length !==1) {
+            alert ("Enter a letter.");
+                    }
+else{
+
+}
+   
 var wins = 0;
 var losses = 0;
 
