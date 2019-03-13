75e66fdb410563a204efad199ba23fdca1bf5ff3
Tori Fortune <toriolley@gmail.com>
2019-03-13 07:29:17 -0500

    Please enter the commit message for your changes. Lines starting

--- assets/javascript/sleuthing.js
+++ assets/javascript/sleuthing.js
@@ -1,8 +1,20 @@
 //The object of the game is to correctly guess and spell out a show from the 80s....die hard shepherd 
+//created html doc
 
-alert("Would you like to walk down memory lane?");
-alert("You have 15 total guesses to name this show!")
 
+alert("You have 15 total guesses to name this show!");
+
+var name = prompt ("What is your name?");
+console.log ("Hi " +  "there " + name);
+var memorylane = confirm ("Would you like to walk down memory lane?");
+if (memorylane)
+{
+    alert("Great, have fun!");
+    }
+    
+    else {
+        alert("Too bad, maybe next time!")
+    }
 var wins = 0;
 var losses = 0;
 
